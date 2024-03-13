2d13d7f162d4347cfb869203b4b3b1642e540175
LotusFlower77 <110903249+LotusFlower77@users.noreply.github.com>
2024-03-13 23:45:14 +0900

    240313_4

--- srcs/requirements/mariadb/setup.sh
+++ srcs/requirements/mariadb/setup.sh
@@ -7,5 +7,5 @@ sed -i 's/skip-networking/#skip-networking/' /etc/my.cnf.d/mariadb-server.cnf
 sed -i 's/#bind-address/bind-address/' /etc/my.cnf.d/mariadb-server.cnf
 
 mariadb-install-db --user=mysql --datadir=/var/lib/mysql
-mariadbd-safe &
+mariadbd-safe & sleep 1
 mariadb < /root/wpdb.sql
